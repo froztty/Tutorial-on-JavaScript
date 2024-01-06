68bf6c88ff9dfac62020d555c10cfcdf7535e1b8
froztty <91167567+froztty@users.noreply.github.com>
2024-01-05 20:29:01 -0800

    Math Objects
    Learning and writing notes for all the math objects. Also have contents for Math.random() and how to do it correctly.

---
+++ Random Num Program/index.js
@@ -0,0 +1,43 @@
+
+//Math = built-in object that provides a 
+//       collection of properties and methods
+
+console.log(Math.PI); //gives pi
+let x = 3.99
+let y = 2
+let z;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x, y); // x ^ y
+z = Math.sqrt(x);
+z = Math.log(x);
+// there is also sin, cos, and tan
+// abs, max, min
+
+console.log(z);
+
+let randomNum = Math.random();
+
+// if we wanna roll a dice between 0-6
+randomNum = Math.floor(Math.random() * 6);
+// by adding one we can have it between 1-6 
+//randomNum = Math.floor(Math.random() * 6) + 1;
+
+//so we can have it between 1-100
+//randomNum = Math.floor(Math.random() * 6) + 1;
+
+const min = 50;
+const max = 100;
+
+//the problem with doing this is we are multiplying it by 100 and adding 50
+//we need to have it between 50-100
+//randomNum = Math.floor(Math.random() * max) + min;
+randomNum = Math.floor(Math.random() * (max- min)) + min;
+//now we can have it correct
+
+
+
+console.log(randomNum);
